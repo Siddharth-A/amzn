3740b117e6b18c441ef99bd1a9b1fb5576a918cd
Siddharth-A <siddharth.ahluwalia16@gmail.com>
2021-09-10 10:47:56 -0400

    [STACK] maximal area in rectangle complete

--- stack/8stack.cpp
+++ stack/8stack.cpp
@@ -13,11 +13,125 @@
 #include <stack>
 #include <algorithm>
 
+#define ppi pair<int,int>
+
 using namespace std;
 
+int maxareahist(vector<int>& input){
+    // NSL
+    vector<int> left;
+    vector<int> right;
+    stack<ppi> s;
+    vector<int> sol;
+    int area=0;
+    int temp=0;
+
+    for (int i=0;i<input.size();i++){
+        // stack null
+        if(s.size()==0){
+            left.push_back(-1);
+        }
+        // s.top()<input[i]
+        else if(s.top().first<input[i]){
+            left.push_back(s.top().second);
+        }
+        // s.top()>input[i]
+        else if(s.top().first>=input[i]){
+            while(s.size()>0 && s.top().first>=input[i]){
+                s.pop();
+                if(s.size()==0){
+                    left.push_back(-1);
+                }
+                else if(s.top().first<input[i]){
+                    left.push_back(s.top().second);
+                }
+            }
+        }
+        s.push(make_pair(input[i],i));
+    }
+
+    while(s.size()>0){
+        s.pop();
+    }
+
+    for (int i=input.size()-1;i>=0;i--){
+        // stack null
+        if(s.size()==0){
+            right.push_back(input.size());
+        }
+        // s.top()<input[i]
+        else if(s.top().first<input[i]){
+            right.push_back(s.top().second);
+        }
+        // s.top()>input[i]
+        else if(s.top().first>=input[i]){
+            while(s.size()>0 && s.top().first>=input[i]){
+                s.pop();
+                if(s.size()==0){
+                    right.push_back(input.size());
+                }
+                else if(s.top().first<input[i]){
+                    right.push_back(s.top().second);
+                }
+            }
+        }
+        s.push(make_pair(input[i],i));
+    }
+    reverse(right.begin(),right.end());
+
+    // width = (right-left)-1
+    // area = width*input[i]
+    for (int x=0;x<left.size();x++){
+        area=((right[x]-left[x])-1)*input[x];
+        sol.push_back(area);
+        // cout << sol[x] << " ";
+        if (temp<sol[x])
+            temp=sol[x];
+    }
+    
+    return temp;
+}
+
 int main(){
-    int input[4][4] = {0,1,1,0,
-                       1,1,1,1,
-                       1,1,1,1,
-                       1,1,0,0};
+    
+    vector<vector<int>> input{
+        {0,1,1,0},
+        {1,1,1,1},
+        {1,1,1,1},
+        {1,1,0,0}
+    };
+
+    vector<int> h;
+    int max=0;
+
+    for(int i=0;i<input.size();i++){
+        for(int j=0;j<input[i].size();j++){
+            // if first line of matrix
+            if(i==0){
+                cout << "l0 ";
+                h.push_back(input[0][j]);
+                cout << h[j] << " ";
+            }
+            // if line is not first line
+            else if(i>0){
+                if(input[i][j]==0){
+                    cout << "l" << i << " ";
+                    h[j] = 0;
+                }
+                
+                else if (input[i][j]!=0){
+                    cout << "l" << i << " ";
+                    h[j] = h[j]+input[i][j];
+                }
+                    
+                cout << h[j] << " ";
+            }
+        }
+        if(max<maxareahist(h)){
+            max = maxareahist(h);
+        }
+        cout << "\n";
+    }
+    cout << max << endl;
+    return 0;
 }
