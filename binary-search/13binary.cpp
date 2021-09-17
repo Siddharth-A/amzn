2e71979066b954fec7b672962a2b88dd3a9d8ec4
Siddharth-A <siddharth.ahluwalia16@gmail.com>
2021-09-17 16:08:44 -0400

    [BS] initial commit for 13binary.cpp

---
+++ binary-search/13binary.cpp
@@ -0,0 +1,14 @@
+// position of element in infinite sorted list: 
+// https://www.geeksforgeeks.org/find-position-element-sorted-array-infinite-numbers/
+
+// 
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
