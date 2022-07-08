71075b75ab92724f630d8437c67bf61ecc530be3
Siddharth-A <siddharth.ahluwalia16@gmail.com>
2022-07-07 22:51:24 -0400

    [STACK] solve stack questions 3-4

--- stack/stack-practice.cpp
+++ stack/stack-practice.cpp
@@ -5,6 +5,8 @@
 // 5 https://leetcode.com/problems/online-stock-span/
 // 
 
+// NGR: nearest greatest right NGE: next greatest element
+// NGL: nearest greatest left  PGE: previous greatest element
 
 #include <functional>
 #include <queue>
@@ -79,21 +81,74 @@ void previousgreatestelement(vector<int> input){
     
 }
 
+void previoussmallestelement(vector<int> input){
+    stack<int> s;
+    vector<int> sol;
+
+    for(int i=0;i<=input.size()-1;i++){
+        // case 1:stack empty
+        if(s.size()==0)
+            sol.push_back(-1);
+
+        // case 2:stack top < input[i]
+        else if(s.size()>0 && s.top()<input[i])
+            sol.push_back(s.top());
+
+        // case 3: stack top > input[i]
+        else if(s.size()>0 && s.top()>input[i]){
+            while(s.size()>0 && s.top()>input[i])
+                s.pop();
+            if(s.size()==0)
+                sol.push_back(-1);
+            else if(s.top()<input[i])
+                sol.push_back(s.top());
+        }
+        s.push(input[i]);
+    }
+    for(auto i:sol)
+        cout << i << " ";
+    cout << endl;
+}
+
 void nextsmallestelement(vector<int> input){
     stack<int> s;
     vector<int> sol;
 
     for(int i=input.size()-1;i>=0;i--){
+        // case 1: stack empty
+        if(s.size() == 0)
+            sol.push_back(-1);
 
+        // case 2: stack top < input
+        else if(s.top() < input[i] && s.size()>0)
+            sol.push_back(s.top());
+
+        // case 3: stack top > input
+        else if(s.size()>0 && s.top()>=input[i]){
+            while(s.size()>0 && s.top()>=input[i])
+                s.pop();
+
+            if(s.size()==0)
+                sol.push_back(-1);
+
+            else if(s.top()<input[i])
+                sol.push_back(s.top());
+
+        }
         s.push(input[i]);
     }
+    reverse(sol.begin(),sol.end());
+    for (auto x:sol)
+        cout << x << " ";
+    cout << endl;
 }
 
 int main()
 {
     vector<int> input1={1,3,0,0,1,2,4};
-    // nextgreatestelement(input1);         //1 (NGR)(start from reverse array)
-    // previousgreatestelement(input1);     //2 (NGL)
-    nextsmallestelement(input1);    //4
+    // nextgreatestelement(input1);         //1 (NGR)(NGE)(start from reverse array)
+    // previousgreatestelement(input1);     //2 (NGL)(PGE)
+    previoussmallestelement(input1);     //3 (NSL)(PSE)
+    // nextsmallestelement(input1);         //4 (NSR)(NSE)(start from reverse array)
     return 0;
 }
