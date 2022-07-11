b39976bc364bca5ce565cc11234dcb53d3a522d7
Siddharth-A <siddharth.ahluwalia16@gmail.com>
2022-07-10 22:34:10 -0400

    [STACK] solve stack question 6

--- stack/stack-practice.cpp
+++ stack/stack-practice.cpp
@@ -3,7 +3,8 @@
 // 3 https://www.geeksforgeeks.org/find-the-nearest-smaller-numbers-on-left-side-in-an-array/
 // 4 https://www.geeksforgeeks.org/next-smaller-element/
 // 5 https://leetcode.com/problems/online-stock-span/
-// 
+// 6 https://leetcode.com/problems/largest-rectangle-in-histogram/
+// 7
 
 // NGR: nearest greatest right (reverse)
 // NGL: nearest greatest left
@@ -29,11 +30,11 @@ void nextgreatestelement(vector<int> input){
         // case1: stack empty
         if(s.size()==0)
             sol.push_back(-1);
-        
+
         // case2: stack top > input
         else if(s.size()>0 && s.top()>input[i])
             sol.push_back(s.top());
-        
+
         // case3: stack top < input
         else if(s.top()<=input[i] && s.size()>0){
             while(s.top()<=input[i] && s.size()>0)
@@ -62,10 +63,10 @@ void previousgreatestelement(vector<int> input){
     for(int i=0;i<=input.size()-1;i++){
         if(s.size()==0)
             sol.push_back(-1);
-        
+
         else if(s.top()>input[i])
             sol.push_back(s.top());
-        
+
         else if(s.size()>0 && s.top()<=input[i]){
             while(s.size()>0 && s.top()<=input[i])
                 s.pop();
@@ -79,7 +80,7 @@ void previousgreatestelement(vector<int> input){
     for(auto i:sol)
         cout << i << " ";
     cout << endl;
-    
+
 }
 
 void previoussmallestelement(vector<int> input){
@@ -177,15 +178,93 @@ void stockspanproblem(vector<int> input){
     cout << endl;
 
 }
+
+void maximumareahistogram(vector<int> input){
+    // create NSL and NSR vectors
+    // points where the current bar discontinues = width = (NSR-NSL-1)
+    // area = width*input[i]
+    vector<int> sol;
+    vector<int> left;
+    vector<int> right;
+    vector<int> area;
+    stack<pair<int,int>> s;
+    stack<pair<int,int>> r;
+    int max=0;
+
+    // part1: NSL
+    for(int i=0;i<input.size();i++){
+        if(s.size()==0)
+            left.push_back(-1);
+
+        else if(s.size()>0 && s.top().first < input[i])
+            left.push_back(s.top().second);
+
+        else if(s.size()>0 && s.top().first >= input[i]){
+            while(s.size()>0 && s.top().first >= input[i])
+                s.pop();
+            if(s.size()==0)
+                left.push_back(-1);
+            else if(s.top().first < input[i])
+                left.push_back(s.top().second);
+        }
+        s.push(make_pair(input[i],i));
+    }
+    // part2: NSR
+    for(int i=input.size()-1;i>=0;i--){
+        if(r.size()==0)
+            right.push_back(input.size()); //inportant detail
+
+        else if(r.size()>0 && r.top().first < input[i])
+            right.push_back(r.top().second);
+
+        else if(r.size()>0 && r.top().first >= input[i]){
+            while(r.size()>0 && r.top().first >= input[i])
+                r.pop();
+            if(r.size()==0)
+                right.push_back(input.size());
+            else if(r.top().first < input[i])
+                right.push_back(r.top().second);
+        }
+        r.push(make_pair(input[i],i));
+    }
+    reverse(right.begin(), right.end());
+
+    // part3: area
+    for(int i=0;i<left.size();i++){
+        area.push_back((right[i] - left[i] - 1) * input[i]);
+    }
+
+    cout << "LEFT VECTOR:"<< endl;
+    for (int x=0;x<left.size();x++){
+        cout << left[x] << " ";
+    }
+    cout << endl;
+    cout << "RIGHT VECTOR:"<< endl;
+    for (int x=0;x<left.size();x++){
+        cout << right[x] << " ";
+    }
+    cout << endl;
+    cout << "AREA VECTOR:"<< endl;
+    for (int x=0;x<left.size();x++){
+        cout << area[x] << " ";
+        if(area[x]>max)
+            max=area[x];
+    }
+    cout << endl;
+
+}
+
 int main()
 {
     vector<int> input1={1,3,0,0,1,2,4};
     vector<int> input2={100,80,60,70,60,75,85};
+    vector<int> input3={6,2,5,4,5,1,6};
 
     // nextgreatestelement(input1);         //1 (NGR)(NGE)(start from reverse array)
     // previousgreatestelement(input1);     //2 (NGL)(PGE)
     // previoussmallestelement(input1);     //3 (NSL)(PSE)
     // nextsmallestelement(input1);         //4 (NSR)(NSE)(start from reverse array)
     // stockspanproblem(input2);            //5
+    maximumareahistogram(input3);       //6
     return 0;
 }
